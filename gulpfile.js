d4920bae1f41fad62139f6a7d17d615e6dfaf48d
GaneshSAcharya <31121986g@gmail.com>
2018-06-18 08:03:28 +0530

    added a gulpfile to automate the js build using run-command plugin for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-const gulp = require('gulp')
-const shell = require('gulp-shell')
- 
-gulp.task('example', () => {
-  return gulp.src('*.js', {read: false})
-  .pipe(shell([
-    'echo <%= file.path %>'
-  ]))
-})
+var gulp = require('gulp')
+var run = require('gulp-run-command').default;
 
-gulp.task('default', ['example']);
+gulp.task('check', run('node main.js', {
+    env: { NODE_ENV: 'production' }
+}))
+  
