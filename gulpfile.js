8b8da601f0531a1f02f7ed4b68c2fbf970f33314
GaneshSAcharya <31121986g@gmail.com>
2018-06-18 07:45:04 +0530

    Tried adding a gulpfile to automate the JS run from shell

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp')
+const shell = require('gulp-shell')
+ 
+gulp.task('example', () => {
+  return gulp.src('*.js', {read: false})
+  .pipe(shell([
+    'echo <%= file.path %>'
+  ]))
+})
+
+gulp.task('default', ['example']);
---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello from main.js")
+
+var name = 'Ganesh', age = 31, isMarried = false;
+
+console.log(name + "/" + age + "/" + isMarried);
+
+console.log(age + age + isMarried);
+
+console.log(name + isMarried);
