965bb961203a3b2bf76e69a945d733d7da210027
GaneshSAcharya <31121986g@gmail.com>
2018-06-25 11:51:25 +0530

    Initial Inheritance learning in JS

--- inheritance.js
+++ inheritance.js
@@ -1,7 +1,7 @@
 
 var Person = function(name, age) {
     this.name = name,
-    this.age = age;
+    this.age = age
 }
 
 console.log(Person);
@@ -12,18 +12,24 @@ console.log(Person);
 
 var Ganesh = new Person('Ganesh', 31);
 
-Person.prototype.salutation = function() {
-    return 'Hello, I am ' + this.name + ' of Age ' + this.age ;
-};
+// Person.prototype.salutation = function() {
+//     return 'Hello, I am ' + this.name + ' of Age ' + this.age ;
+// };
 
-console.log(Ganesh.salutation());
+// console.log(Ganesh.salutation());
+
+// var Reshma = new Person('Reshma', 28);
+
+Person.prototype.sex = 'Male';
+
+// Ganesh.sex = 'Male';
+
+console.log(Ganesh.sex);
 
 var Reshma = new Person('Reshma', 28);
+Reshma.sex = 'Female';
 
-console.log(Person);
-
-
-
+console.log(Reshma.sex);
 // var Risk = function (insuredName, inceptionYear, quotingCurrency) {
 //     this.insuredName = insuredName,
 //     this.inceptionYear = inceptionYear,
