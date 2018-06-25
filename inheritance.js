48521dfcc71b6f34b98657b5947cae5240f282ba
GaneshSAcharya <31121986g@gmail.com>
2018-06-25 18:12:46 +0530

    Premitive and Objects

--- inheritance.js
+++ inheritance.js
@@ -1,35 +1,35 @@
 
-var Person = function(name, age) {
-    this.name = name,
-    this.age = age
-}
-
-console.log(Person);
-
-// Person.sex = 'Male';
+// var Person = function(name, age) {
+//     this.name = name,
+//     this.age = age
+// }
 
 // console.log(Person);
 
-var Ganesh = new Person('Ganesh', 31);
+// // Person.sex = 'Male';
 
-// Person.prototype.salutation = function() {
-//     return 'Hello, I am ' + this.name + ' of Age ' + this.age ;
-// };
+// // console.log(Person);
 
-// console.log(Ganesh.salutation());
+// var Ganesh = new Person('Ganesh', 31);
+
+// // Person.prototype.salutation = function() {
+// //     return 'Hello, I am ' + this.name + ' of Age ' + this.age ;
+// // };
+
+// // console.log(Ganesh.salutation());
+
+// // var Reshma = new Person('Reshma', 28);
+
+// Person.prototype.sex = 'Male';
+
+// // Ganesh.sex = 'Male';
+
+// console.log(Ganesh.sex);
 
 // var Reshma = new Person('Reshma', 28);
+// Reshma.sex = 'Female';
 
-Person.prototype.sex = 'Male';
-
-// Ganesh.sex = 'Male';
-
-console.log(Ganesh.sex);
-
-var Reshma = new Person('Reshma', 28);
-Reshma.sex = 'Female';
-
-console.log(Reshma.sex);
+// console.log(Reshma.sex);
 // var Risk = function (insuredName, inceptionYear, quotingCurrency) {
 //     this.insuredName = insuredName,
 //     this.inceptionYear = inceptionYear,
@@ -43,3 +43,23 @@ console.log(Reshma.sex);
 // var subex = new Risk('Subex Azure', 2018, 'USD');
 
 // console.log(subex.renewable());
+
+// var Person = function(name, yearOfBirth) {
+
+//     this.name = name,
+//     this.yearOfBirth = yearOfBirth;
+
+// };
+
+// var suvodip = new Person('Suvodip', 1988);
+
+// var suvodipJudwa = suvodip;
+
+// console.log(suvodipJudwa.yearOfBirth);
+
+// suvodip.yearOfBirth = 1987;
+
+// console.log(suvodipJudwa.yearOfBirth);
+
+
+
