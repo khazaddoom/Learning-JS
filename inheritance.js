00ab7ee346b93686d3b7086dd995331a207116e0
GaneshSAcharya <31121986g@gmail.com>
2018-06-25 11:34:32 +0530

    Learning inheritance concepts in JS

---
+++ inheritance.js
@@ -0,0 +1,14 @@
+
+var Risk = function (insuredName, inceptionYear, quotingCurrency) {
+    this.insuredName = insuredName,
+    this.inceptionYear = inceptionYear,
+    this.quotingCurrency = quotingCurrency,
+    allowRenewal = false,
+    this.renewable = function() {
+        allowRenewal === false? return 'Renewable' :  return 'Not Renewable' ;
+    }
+}
+
+var subex = new Risk('Subex Azure', 2018, 'USD');
+
+console.log(subex.renewable());
